870ac8e4ca36ab39b5a4304c888eadb245ad1fe5
Arman SIngh <armanprabhat123912@gmail.com>
2025-07-06 18:07:24 +0530

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',   // Important: Root path
   plugins: [react()],
-  base: '/urja-mobility-final/',
 });
