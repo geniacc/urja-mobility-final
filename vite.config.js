5ffdceb362ed30b0082d13e19ccfd0115bc12807
Arman SIngh <armanprabhat123912@gmail.com>
2025-07-06 20:12:25 +0530

    Update vite.config.js and other fixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
 
 export default defineConfig({
-  base: '/',   // Important: Root path
-  plugins: [react()],
+  base: isGitHubPages ? '/urja-mobility-final/' : '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [{ src: 'CNAME', dest: '.' }],
+    }),
+  ],
 });
