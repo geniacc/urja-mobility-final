b4bee6bdc755603c595caf68031e0f5ad2530a11
Arman SIngh <armanprabhat123912@gmail.com>
2025-07-06 18:28:31 +0530

    Deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/urja-mobility-final/',
+  base: '/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'CNAME', dest: '.' }
+      ]
+    }),
+  ],
 });
